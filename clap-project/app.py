19109cf0c87e2039ec3cd81c42f82a56a8af5449
Krish Sharma <krishsharma3355@gmail.com>
2026-07-03 11:59:05 +0530

    sounddevice working handling

--- clap-project/app.py
+++ clap-project/app.py
@@ -3,3 +3,21 @@ import sounddevice as sd
 import time
 import subprocess
 
+THRESHOLD = 10000
+MIN_DELAY = 0.2
+MAX_DELAY = 1.0
+CHUNK = 1024
+DTYPE = 'int16'
+CHANNEL = 1
+RATE  = 44100
+
+chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+stream = sd.InputStream(
+    samplerate = RATE,
+    channels = CHANNEL,
+    dtype = DTYPE,
+    blocksize = CHUNK
+)
+
+stream.start()

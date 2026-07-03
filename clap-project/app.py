6bc781913065ba49b9679fa8ef3a53e42cf0592f
Krish Sharma <krishsharma3355@gmail.com>
2026-07-03 21:15:39 +0530

    Clapping system work

--- clap-project/app.py
+++ clap-project/app.py
@@ -23,11 +23,10 @@ def open_chrome(url):
 def open_youtube():
     open_chrome("https://www.youtube.com")
 
-def open_vscode(vccode_path):
+def open_vscode():
     subprocess.Popen([
         vscode_path
     ])
-
 
 stream = sd.InputStream(
     samplerate = RATE,
@@ -35,5 +34,50 @@ stream = sd.InputStream(
     dtype = DTYPE,
     blocksize = CHUNK
 )
-
 stream.start()
+
+print("Listening for 1 clap to open 'Chrome', 2 to open 'Vs Code', 3 to open 'Youtube' and Multiple claps to open all in a time (Press Ctrl+C to stop)")
+
+last_clap_time = 0
+clap_count = 0
+
+try:
+    while True:
+        data, overflowed = stream.read(CHUNK)
+        peak = np.abs(data).max()
+        current_time = time.time()
+
+        if peak > THRESHOLD and (current_time - last_clap_time) > MIN_DELAY:
+            clap_count += 1
+            last_clap_time= current_time
+            print(f"Clap detected! (Count: {clap_count})")
+
+        if clap_count > 0 and (current_time - last_clap_time) > MAX_DELAY:
+
+            if clap_count == 1:
+                    print("Single clap detected! Opening Chrome...")
+                    open_chrome("https://www.google.com")
+                    
+            elif clap_count == 2:
+                    print("Double clap detected! Opening VS Code...")
+                    open_vscode()
+                    
+            elif clap_count == 3:
+                    print("Triple clap detected! Opening Youtube...")
+                    open_youtube()
+                   
+            else:
+                print("Multiple claps detected! Opening everything...")
+                open_chrome("https://www.google.com")
+                open_vscode()
+                open_youtube()
+            
+            clap_count = 0
+            print("Workspace active. Pausing Friday microphone so music doesn't trigger it...")
+            time.sleep(14400)
+
+except KeyboardInterrupt:
+    print("Friday is shutting down...")
+finally:
+    stream.stop()
+    stream.close()
